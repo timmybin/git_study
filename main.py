5aeac7f9148c22db6a525407a2c49f2594ce7e5e
laihaibin <laihaibin@sensetime.com>
2019-06-19 10:24:44 +0800

    add a mul(a, b) function

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ print ("Life is short, you need Python !")
 a = 10
 b = 20
 
-def sum(a, b):
-    return (a+b)
-
-c = sum(a, b)
+c = a + b
 print("The value of c is  ", c)
+
+def mul(a, b):
+    return (a * b)
