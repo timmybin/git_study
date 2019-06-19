7423ae060a05c830e058abc13d9ef6110a0ec0eb
laihaibin <laihaibin@sensetime.com>
2019-06-19 10:16:55 +0800

    add a new function sum(a,b)

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ print ("Life is short, you need Python !")
 
 a = 10
 b = 20
-c = a + b
-printf("The value of c is ",c)
+
+def sum(a, b):
+    return (a+b)
+
+c = sum(a, b)
+print("The value of c is  ", c)
